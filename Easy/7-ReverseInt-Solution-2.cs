a2087f7b22d77acb3f5dc020fba1ac938d343ee8
Dizzie42 <dohrann@gmail.com>
2019-06-22 10:20:59 -0500

    Fixed overflow issues

--- Easy/7-ReverseInt-Solution-2.cs
+++ Easy/7-ReverseInt-Solution-2.cs
@@ -1,18 +1,18 @@
 /*
   Given a 32-bit signed int, reverse the digits
-  --Time of O(log(n)) and space of O(1)
+  --Time of O(log(n)) (36ms < 98.95% of solutions) and space of O(1) (< 100% of submitted solutions)
 */
 
 public class Solution {
     public int Reverse(int x) {
-      int r = 0;
+      double r = 0;                                             //We'll manually handle int32 min/max values
 
       while(x != 0){
         r = r * 10 + (x%10);
         x /= 10;
       }
 
-      if(r < Int32.MinValue || r > Int32.MaxValue) return 0;
-      return r;
+      if(r < Int32.MinValue || r > Int32.MaxValue) return 0;    //Manually handle int32 min/max
+      return (int) r;
     }
 }
